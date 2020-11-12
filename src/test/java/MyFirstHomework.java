c60ba178c9993810afb8f11a02d436ba741f09c3
Jevgenijs93 <jevgenijs.sinkevics93@gmail.com>
2020-11-12 19:41:31 +0200

    My first commit

---
+++ src/test/java/MyFirstHomework.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+public class MyFirstHomework {
+
+    @Test
+    public void first() {
+        System.out.println("Hello, corona!");
+        int sum = (sumTwoDigits(15, 20));
+        System.out.println("Sum is: " + sum);
+        System.out.println(sumTwoDigits(20, 25)); // CTRL = / (comment row or many rows)
+        age = 19;
+        String e = "Hello, world!";
+        String b = e.replace(" ", "");
+        int  a = b.length();
+        System.out.println(a);
+        String[] c = e.split(" ");
+        System.out.println(c.length);
+    }
+}
---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class MyFirstTest {
+    private int globalVar = 10;
+    Integer age = 18;
+
+    //  BigDecimal price = new BigDecimal("10.01");
+/*
+Many
+Row
+Comment
+ */
+    // One Row comment
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, corona!");
+        int sum = (sumTwoDigits(15, 20));
+        System.out.println("Sum is: " + sum);
+        System.out.println(sumTwoDigits(20, 25)); // CTRL = / (comment row or many rows)
+        age = 19;
+        String e = "Hello, world!";
+        String b = e.replace(" ", "");
+        int  a = b.length();
+        System.out.println(a);
+        String[] c = e.split(" ");
+        System.out.println(c.length);
+    }
+
+    private int sumTwoDigits(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+}
