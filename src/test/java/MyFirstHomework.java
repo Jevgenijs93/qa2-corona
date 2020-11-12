48f000136111a5189004e0fabb60ecb92f9821ba
Jevgenijs93 <jevgenijs.sinkevics93@gmail.com>
2020-11-12 20:00:13 +0200

    some changes to test commit/push process

--- src/test/java/MyFirstHomework.java
+++ src/test/java/MyFirstHomework.java
@@ -3,17 +3,21 @@ import org.junit.jupiter.api.Test;
 public class MyFirstHomework {
 
     @Test
-    public void first() {
+    public void firstTask() {
         System.out.println("Hello, corona!");
-        int sum = (sumTwoDigits(15, 20));
-        System.out.println("Sum is: " + sum);
-        System.out.println(sumTwoDigits(20, 25)); // CTRL = / (comment row or many rows)
-        age = 19;
-        String e = "Hello, world!";
+        int startSum = 85000;
+        double percentSum = 0.24 ;
+        //float percentSum = (countPercentSum(85000, 24));
+
+    /*    String e = "Hello, world!";
         String b = e.replace(" ", "");
         int  a = b.length();
         System.out.println(a);
         String[] c = e.split(" ");
-        System.out.println(c.length);
+        System.out.println(c.length); */
     }
+
+    private float countPercentSum(int a, float b) {
+        float c = a * b;
+        return c;
 }
