acd3ea51c102fda19a9a6add8bd88b7162c8ba0f
Jevgenijs93 <jevgenijs.sinkevics93@gmail.com>
2020-12-03 20:55:00 +0200

    Lesson 8 code

--- src/test/java/DelfiTests.java
+++ src/test/java/DelfiTests.java
@@ -6,8 +6,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class DelfiTests {
     private final By ACCEPT_COOKIE_BTN = By.xpath(".//button[@mode = 'primary']");
+    private final By ARTICAL_TITLE = By.xpath(".//span[@itemprop= 'headline name']");
 
     @Test
     public void firstDelfiTest() {
@@ -21,5 +24,36 @@ public class DelfiTests {
 
         WebElement acceptBtn = driver.findElement(ACCEPT_COOKIE_BTN);
         acceptBtn.click();
+    }
+
+    @Test
+    public void tvnetTest() {
+        String articleToOpen = "Pūce strādās divās Saeimas komisijās";
+
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://tvnet.lv");
+
+        List<WebElement> articles = driver.findElements(ARTICAL_TITLE); // find all articles
+
+  /*      for (int i = 0; i < articles.size(); i = i + 1) {
+            String currentTitle = articles.get(i).getText();
+            if (currentTitle.startsWith(articleToOpen)) {
+                articles.get(i).click();
+                break;
+            }
+//            System.out.println(i + ". " + articles.get(i).getText()); // need for print out name of all articles
+        } */
+        //------------------------------------------foreach--------------------------------------------------------
+        for (WebElement we : articles) {
+            if (we.getText().startsWith(articleToOpen)) {
+                we.click();
+                break;
+            }
+
+            //  System.out.println(we.getText());
+        }
     }
 }
