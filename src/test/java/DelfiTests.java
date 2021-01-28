6fa01ed6421c46717379ddb202a3a731cb92e5a3
Jevgenijs93 <jevgenijs.sinkevics93@gmail.com>
2021-01-28 21:05:10 +0200

    11th lection

--- src/test/java/DelfiTests.java
+++ src/test/java/DelfiTests.java
@@ -1,3 +1,5 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,18 +12,31 @@ import java.util.List;
 
 public class DelfiTests {
     private final By ACCEPT_COOKIE_BTN = By.xpath(".//button[@mode = 'primary']");
-    private final By ARTICAL_TITLE = By.xpath(".//span[@itemprop= 'headline name']");
+    private final By ARTICLE_TITLE = By.xpath(".//span[@itemprop= 'headline name']");
+
+    private final Logger LOGGER = LogManager.getLogger(DelfiTests.class);
 
     @Test
     public void firstDelfiTest() {
-        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe"); // set parameter -  key, path (system (only one) - static object)
+        LOGGER.info("This test will check accept cookie btn");
+
+        LOGGER.info("Setting up driver path");
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");// set parameter -  key, path (system (only one) - static object)
+
+        LOGGER.info("Opening browser window");
         WebDriver driver = new ChromeDriver(); // run Chrome (WebDriver - object, which responds to work with browser window)
+
+        LOGGER.info("Maximizing window");
         driver.manage().window().maximize();
+
+        LOGGER.info("Open Delfi page");
         driver.get("http://delfi.lv"); // lead to link (get)
 
+        LOGGER.info("Waiting for accept cookies modal window");
         WebDriverWait wait = new WebDriverWait(driver, 10); // max time wait, after that fail test
         wait.until(ExpectedConditions.visibilityOfElementLocated(ACCEPT_COOKIE_BTN)); // until button will be visible
 
+        LOGGER.info("Pressing Accept btn");
         WebElement acceptBtn = driver.findElement(ACCEPT_COOKIE_BTN);
         acceptBtn.click();
     }
@@ -36,7 +51,7 @@ public class DelfiTests {
 
         driver.get("http://tvnet.lv");
 
-        List<WebElement> articles = driver.findElements(ARTICAL_TITLE); // find all articles
+        List<WebElement> articles = driver.findElements(ARTICLE_TITLE); // find all articles
 
   /*      for (int i = 0; i < articles.size(); i = i + 1) {
             String currentTitle = articles.get(i).getText();
