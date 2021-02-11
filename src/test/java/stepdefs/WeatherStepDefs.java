6bbbdce17383ce3e3d2e773ea50b86ff997f6958
Jevgenijs93 <jevgenijs.sinkevics93@gmail.com>
2021-02-11 20:59:32 +0200

    13th lesson

---
+++ src/test/java/stepdefs/WeatherStepDefs.java
@@ -0,0 +1,30 @@
+package stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Test;
+
+public class WeatherStepDefs {
+    @Given("city id: {int}")
+    public void set_city_id(int cityID) {
+
+    }
+
+    @When("we are requesting weather data")
+    public void request_weather() {
+
+    }
+
+    @Then("lon is: {double}")
+    public void check_lon(double lon) {
+
+    }
+
+    @Then("lat is: {double}")
+    public void check_lat(double lat) {
+
+    }
+
+
+}
